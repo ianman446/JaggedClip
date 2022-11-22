5b7883780248bde9b821462b778a73ffc3862e7f
Ian <ianman446@gmail.com>
2022-11-22 01:58:04 -0700

    2/3
    more to push.

---
+++ JaggedClip/Source/JaggedClip/JaggedClipGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "JaggedClipGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class JAGGEDCLIP_API AJaggedClipGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
